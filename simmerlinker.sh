b7f040748fe66fdee16235f476e0051e6833f301
Joe Sepi <sepi@joesepi.com>
2014-12-30 23:45:56 -0500

    updating paths and adding rm for files

--- simmerlinker.sh
+++ simmerlinker.sh
@@ -1,10 +1,11 @@
-homepath='~'
-repopath='~/dotfiles/'
-dotfiles=('.bash_profile' '.bashrc' '.gitconfig' '.gitignore' '.profile' '.vimrc.bundles.local' '.vimrc.local' '.zprofile' '.zshrc')
+homepath="/Users/some-damn-path"
+repopath="$homepath/dotfiles/"
+dotfiles=(".bash_profile" ".bashrc" ".gitconfig" ".gitignore" ".profile" ".vimrc.bundles.local" ".vimrc.local" ".zprofile" ".zshrc")
 
 # TODO backup old files/symlinks
+# rm -rf .bash_profile .bashrc .gitconfig .gitignore .profile .vimrc.bundles.local .vimrc.local .zprofile .zshrc
 
-echo "Dont forget to install oh-my-zsh"
+echo "Dont forget to install oh-my-zsh before running this"
 echo "==> curl -L http://install.ohmyz.sh | sh"
 
 cd "$homepath"
